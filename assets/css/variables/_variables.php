cd6b736f9d09a83ae8fb119860f8467ee34d56b2
tracy apps <t@thisroad.org>
2026-01-17 12:11:08 -0600

    mercantile styles

---
+++ assets/css/variables/_variables.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * CSS variable import: spice late dark theme
+ *
+ */ ?>
+
+<style rel="stylesheet" type="text/css">
+	:root {
+		--dark: #1d1a1a;
+		--dark_green: #082b29;
+		--medium_green: #167a73;
+		--light_green: #94b6a5;
+		--cream: #ede5b6;
+		--light_cream: #f0ecd5;
+		--dark_red: #3b1818;
+		--medium_red: #b52f2f;
+		--light_red: #e0caca;
+		--light_brick: #de9b93;
+		--brick: #c26559;
+		--dark_brick: #944d44;
+		--brown: #8B7355;
+		--orange: #F4A460;
+		--white: #fff;
+
+		--type-sm: clamp(1.26rem, 0.18vi + 1.17rem, 1.48rem);
+		--type-base: clamp(1.6rem, 0.33vi + 1.43rem, 2rem);
+		--type-md: clamp(2.03rem, 0.56vi + 1.75rem, 2.7rem);
+		--type-lg: clamp(2.58rem, 0.89vi + 2.14rem, 3.65rem);
+		--type-xl: clamp(3.28rem, 1.37vi + 2.59rem, 4.92rem);
+		--type-xxl: clamp(4.16rem, 2.07vi + 3.13rem, 6.64rem);
+		--type-xxxl: clamp(5.29rem, 3.07vi + 3.75rem, 8.97rem);
+	}
+
+</style>
